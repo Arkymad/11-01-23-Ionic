0154665acab595c8669be68f820d07254c2ac4cd
Arkymad <phmm2003@hotmail.com>
2023-01-17 18:46:58 -0300

    att

--- 11-01-23/capacitor.config.ts
+++ 11-01-23/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Senac Thalis',
+  appName: '11-01-23',
   webDir: 'www',
   bundledWebRuntime: false
 };
